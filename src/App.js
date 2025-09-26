7ec198451d8eaaab63b2e17729c3c41331ba4abd
SujonRoyBD <sujonroy537527@gmail.com>
2025-09-27 00:03:19 +0600

    domain change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import io from 'socket.io-client';
-const socket = io.connect('http://localhost:8000');
+const socket = io.connect('https://socket-io-ten-phi.vercel.app');
 
 function App() {
   const [msg, setMsg] = useState('')

0db8ffaac8c5591c680f7b6dae9920df6412153f
SujonRoyBD <sujonroy537527@gmail.com>
2025-09-26 23:52:20 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import io from 'socket.io-client';
+const socket = io.connect('http://localhost:8000');
 
 function App() {
+  const [msg, setMsg] = useState('')
+  const [chats, setChats] = useState([])
+
+useEffect(()=>{
+  socket.on('sendmessage',(payload)=>{
+    setChats([...chats, payload])
+  })
+})
+
+  function sendMessage(e){
+    e.preventDefault();
+    socket.emit('sendmessage', {message: msg, userName:"me"});
+    setMsg('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div style={{backgroundColor:"gray",height:"500px"}}>
+    <div style={{marginLeft: '450px', marginTop: '50px',}}>
+    {/*input*/}
+    <div style={{width: "300px"}} >
+      <input style={{marginTop:"30px"}} value={msg} onChange={e=> setMsg(e.target.value)}/>
+
+      <button onClick={sendMessage}>
+send 
+      </button>
     </div>
+     {/*all message show*/}
+<div style={{
+  height: "300px", width:"210px", border:"2px solid white", marginTop:"20px", backgroundColor:"white", 
+}}>
+  {
+    chats.map((payload,index)=>{
+      return(
+        <p key={index}>{payload?.message}</p>
+      )
+    })
+  }
+</div>
+   </div>
+   </div>
   );
 }
 
